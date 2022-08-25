85ae78a09ff468106d6f16090d4cb2ec378ae278
ShubhamJoshii <shubhamjoshii676@gmail.com>
2022-08-25 15:17:34 +0530

    Quotes Generator through API

---
+++ script.js
@@ -0,0 +1,56 @@
+let i=0;
+let dataQ, dataQAuthor;
+const next=() => {
+    i++
+    fetch("https://type.fit/api/quotes")
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data[i]);
+        dataQ = data[i].text
+        dataQAuthor = data[i].author
+        document.getElementById('thoughts').innerHTML=dataQ;
+        document.getElementById('thoughtsAuthor').innerHTML="~ " + dataQAuthor;
+    });
+}
+const previous=() => {
+    i--;
+    fetch("https://type.fit/api/quotes")
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data[i]);
+        dataQ = data[i].text
+        dataQAuthor = data[i].author
+        document.getElementById('thoughts').innerHTML=dataQ;
+        document.getElementById('thoughtsAuthor').innerHTML="~ " + dataQAuthor;
+    });
+}
+
+next();
+let a = 0;
+const changeON = ()=>{
+    if(a%2 == 0){
+        document.getElementById('like').src="./Picture/likeRed.jpg";
+        a++;
+    }
+    else{
+        document.getElementById('like').src="./Picture/like.jpg";
+        a++;
+    }
+}
+
+let b = 0;
+const Save_change = ()=>{
+    if(b%2 == 0){
+        document.getElementById('save').src="./Picture/saveon.png";
+        b++;
+    }
+    else{
+        document.getElementById('save').src="./Picture/save.png";
+        b++;
+
+    }
+}
